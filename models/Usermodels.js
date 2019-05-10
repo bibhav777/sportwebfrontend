4511e9a623bdda0dcc83461b365d926494c668c6
bibhav777 <bibhav.sapkotaqa@gmail.com>
2019-05-11 00:36:34 +0545

    Table user created

--- models/Usermodels.js
+++ models/Usermodels.js
@@ -1,2 +1,40 @@
 var db= require('../configs/databaseconfig');
-console.log(db.sequelize);
+
+const User=db.sequelize.define('user',{
+
+id:{
+	type: db.Sequelize.INTEGER,
+	allowNull: false,
+	autoIncrement: true,
+	primaryKey:true
+},
+
+username: {
+	type: db.Sequelize.STRING,
+	allowNull:false
+},
+
+lastname: {
+	type: db.Sequelize.STRING,
+	allowNull: false
+}
+
+},
+
+//OPTIONS
+{
+
+	freezeTableName:true,
+	tableName:'user'	 
+}
+);
+
+User.sync({force:true})
+.then(function(result){
+   console.log(result);
+
+})
+
+.catch(function(err){
+console.log(err); 
+})
